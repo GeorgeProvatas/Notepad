17f79dfd4e4e28ebc3356ffc924bdeb06e992f13
CoolisCodes <41109210+CoolisCodes@users.noreply.github.com>
2021-01-21 22:01:38 +0200

    Class blueprint
    a good starting point

---
+++ Note.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotePad___
+{
+    class Note
+    {
+        private string myTitle;
+
+        private string myText;
+
+        public string MyTitle 
+        {
+            get 
+            {
+                return myTitle;
+            }
+
+            set 
+            {
+                myTitle = value;
+            }
+        }
+        
+        public string MyText 
+        {
+            get 
+            {
+                return myText;
+            }
+
+            set 
+            {
+                myText = value;
+            }
+        }
+
+        public Note(string title, string text) 
+        {
+            myTitle = title;
+            myText = text;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NotePad___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Note> myNotes = new List<Note>();
+
+            Console.WriteLine("======NOTEPAD======");
+
+            Note newNote = new Note("Skata", "Simera xestika poli!");
+            
+            Note newNote1 = new Note("kaka", "Simera afodefsa poli <3");
+
+            myNotes.Add(newNote);
+            myNotes.Add(newNote1);
+
+            ///////
+            ///
+            
+            for (int i = 0; i < myNotes.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {myNotes[i].MyTitle}");
+            }
+
+            Console.WriteLine("Pleas enter index of note to print");
+
+            while (true) 
+            {
+                int index = 0;
+
+                if (int.TryParse(Console.ReadLine(), out index) && index - 1 < myNotes.Count)
+                {
+                    Console.WriteLine($" its a number");
+
+                    Console.WriteLine($"{myNotes[index - 1].MyTitle}\n{myNotes[index - 1].MyText}");
+
+                    break;
+                }
+
+                break;
+            }
+
+        }
+    }
+}
